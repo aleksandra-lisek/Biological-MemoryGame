53de6b776d92c70e50f6fdded4a81f681700da0b
aleksandra-lisek <olalisek.al@gmail.com>
2017-07-25 12:02:29 +0200

    plansza

---
+++ js/game.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+
+
+
+
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+    constructor() {
+    super();
+    this.state = {
+      squares: Array(36).fill(null),
+    };
+  }
+   handleClick(i) {
+       console.log('tralla');
+
+  }
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]}
+              onClick={() => this.handleClick(i)}/>
+  }
+
+  render() {
+
+    return (
+     <div>
+          <div className='headline'><span>GENETYKA</span></div>
+    <div className="board-container">
+      <div className='board'>
+
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+      </div>
+     </div>
+    <div className='status'><span><b>Liczba ruchów</b></span></div>
+    </div>
+    );
+  }
+}
+
+ class Game extends React.Component {
+    constructor() {
+    super();
+    this.state = {
+    };
+  }
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+// ========================================
+
+export default Game;
